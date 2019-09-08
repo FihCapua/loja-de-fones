cd0a05fa267854d6df5153bb78ec9144fa675fce
FihCapua <fihcapua@gmail.com>
2019-09-07 23:12:24 -0300

    [013] - Desafio Básico - Pegar o elemento botão e mostrar ele no console com uma msg

--- js/description-product.js
+++ js/description-product.js
@@ -1,7 +1,13 @@
 const $heart = window.document.querySelector(".-heart");
+const $buttonSecond = window.document.querySelector(".-second");
 
 $heart.addEventListener("click", handleClick);
+$buttonSecond.addEventListener("click", handleClick);
 
 function handleClick () {
     console.log("ae");
+}
+
+function handleClick () {
+    console.log("Apareci");
 }
