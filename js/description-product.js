196e25dc7103577291587db328f7ccf05e19f2ad
FihCapua <fihcapua@gmail.com>
2019-09-30 16:02:51 -0300

    [014] - Desafio Avançado - Selecionar todas as estrelas

--- js/description-product.js
+++ js/description-product.js
@@ -1,13 +1,34 @@
-const $heart = window.document.querySelector(".-heart");
-const $buttonSecond = window.document.querySelector(".-last");
+const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
 
-$heart.addEventListener("click", handleClick);
-$buttonSecond.addEventListener("click", handleClick);
+$heart.addEventListener("click", handleClickHeart);
 
-function handleClick() {
+$stars[0].addEventListener("click", handleClickStar0);
+$stars[1].addEventListener("click", handleClickStar1);
+$stars[2].addEventListener("click", handleClickStar2);
+$stars[3].addEventListener("click", handleClickStar3);
+$stars[4].addEventListener("click", handleClickStar4);
+
+function handleClickHeart() {
     $heart.classList.toggle("-active");
 }
 
-function funcaoMudaTexto(){
-    document.getElementById("mudatexto").innerHTML = "Carrinho (11)";
+function handleClickStar0() {
+    $stars[0].classList.toggle("-active");
+}
+
+function handleClickStar1() {
+    $stars[1].classList.toggle("-active");
+}
+
+function handleClickStar2() {
+    $stars[2].classList.toggle("-active");
+}
+
+function handleClickStar3() {
+    $stars[3].classList.toggle("-active");
+}
+
+function handleClickStar4() {
+    $stars[4].classList.toggle("-active");
 }
